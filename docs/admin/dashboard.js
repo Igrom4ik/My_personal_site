ba74c36ab77cfc2d928d54c3254fecdc387a7a37
Igor <31959570+Igrom4ik@users.noreply.github.com>
2025-06-03 23:07:59 +0200

    Refactor login to use backend API

--- docs/admin/dashboard.js
+++ docs/admin/dashboard.js
@@ -1,7 +1,9 @@
-if (!localStorage.getItem('isAuthenticated')) {
+if (!localStorage.getItem('token')) {
     window.location.href = 'admin/login.html';
 }
+
 document.getElementById('logout').addEventListener('click', function () {
+    localStorage.removeItem('token');
     localStorage.removeItem('isAuthenticated');
     window.location.href = 'admin/login.html';
 });
